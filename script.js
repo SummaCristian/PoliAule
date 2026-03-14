619b4ca6271f3de27b8e434e427d224adccb70a1
Cristian Summa <cristian.summa@mail.polimi.it>
2026-03-14 19:15:57 +0100

    Added Tab Bar

--- script.js
+++ script.js
@@ -12,3 +12,19 @@ function updateThemeColor(e) {
 }
 
 mq.addEventListener('change', updateThemeColor);
+
+const tabbar = document.querySelector(".tabbar");
+const tabs = document.querySelectorAll(".tab");
+const indicator = document.querySelector(".tab-indicator");
+
+tabbar.style.setProperty("--tabs", tabs.length);
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+
+    document.querySelector(".tab.active")?.classList.remove("active");
+    tab.classList.add("active");
+
+    indicator.style.transform = `translateX(${index * 100}%)`;
+  });
+});
